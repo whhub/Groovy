93a91e3e77af95067825e2cbfcd50e664bd772a7
sam wang <hui.wang@agfa.com>
2020-08-03 09:57:24 +0800

    [groovy] [00009] java command thread

--- JavaCommand.groovy
+++ JavaCommand.groovy
@@ -1,6 +1,7 @@
 final jar_path = 'd:/dev/monorepo/ei-monorepo/asb/tests/webtests-data-server/build/libs/webtests-data-server.jar'
 final cmd = "java -jar $jar_path"
 println cmd
+//cmd.execute()
 //def process 
 //process = cmd.execute()
 final thread = Thread.start {
