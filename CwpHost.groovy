019541fd395ff9359b1274a28c6c610ce97ac0aa
sam wang <hui.wang@agfa.com>
2022-11-30 10:44:07 +0800

    [groovy] [00026] system.getProperty java.io.tmpdir user.home user.dir

--- CwpHost.groovy
+++ CwpHost.groovy
@@ -1,6 +1,8 @@
 static final cwphostFileName = 'cwphost.txt'
 static final cwphost = "eicwpol8-138.med.agfa.be"
-def cwphostFile = new File(System.getProperty("java.io.tmpdir"), cwphostFileName)
+//def cwphostFile = new File(System.getProperty("java.io.tmpdir"), cwphostFileName)
+//def cwphostFile = new File(System.getProperty("user.home"), cwphostFileName)
+def cwphostFile = new File(System.getProperty("user.dir"), cwphostFileName)
 
 if(cwphostFile.exists()) {
     println "${cwphostFile.path} found"
