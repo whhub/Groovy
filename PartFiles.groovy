6803572088ca828a5f3205cc4be0f871ba87ef2f
sam wang <hui.wang@agfa.com>
2022-12-01 14:09:01 +0800

    [groovy] [00029] partFiles

--- PartFiles.groovy
+++ PartFiles.groovy
@@ -1,7 +1,7 @@
 def specDir = new File("D:/dev/ei-monorepo/ei-angular/apps/ei-web-e2e/src/spec/solution")
 def files = specDir.listFiles()
 println files[]
-def parts =  5
+def parts =  '5'.isInteger() ? '5' as Integer : 1
 def fileCount = files.length
 def partSize = fileCount.intdiv(parts)
 println partSize
@@ -11,4 +11,8 @@ for(int part=1; part<=parts; part++) {
 	def start = (part-1) * partSize
 	def end = part == parts ? -1 : part * partSize - 1
 	files[start..end].each{file-> println file.name}
+
+	def specOptions = []
+	files[start..end].each {specOptions << "--specs=${it.getAbsolutePath()}" }
+	println specOptions.join(' ')
 }
